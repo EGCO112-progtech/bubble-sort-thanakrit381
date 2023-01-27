3e63eb420a266dfd0c06b444914bdb8bba2acff8
thanakrit381 <123150665+thanakrit381@users.noreply.github.com>
2023-01-27 10:21:31 +0000

    yyy

--- .ccls-cache/@home@runner@bubble-sort-thanakrit381/sorting.h
+++ .ccls-cache/@home@runner@bubble-sort-thanakrit381/sorting.h
@@ -42,15 +42,3 @@ void bubbleSort(int a[], int n) {
     if (sorted == 0) break;
   }
 }
-
-
-int Isprime(int x)
-{
-  if (x == 0 || x == 1) return 0;
-
-  for (int i = 2; i < x; i++)
-    {
-      if (x % i == 0) return 0;
-    }
-  return 1;
-}
--- main.c
+++ main.c
@@ -1,18 +1,29 @@
-#define N 6
+#include <stdlib.h>
 #include <stdio.h>
-
 #include "sorting.h"
-
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
-
-   display(a,N);
-  bubbleSort(a,N); 
+int main(int argc, char *argv[]) {
+int i,*a,N,j=0, n;
+  
+N=argc-1;
+a=(int*)malloc(sizeof(int)*N);
+int* prime =(int*)malloc(sizeof(int)*N);
+for(i=0;i<N;i++){
+  a[i]=atoi(argv[i+1]);
+  if(Isprime(a[i])){
+    prime[j]=a[i];
+    j++;
+    }
+  }
+  n=j;
+  if(n==0)printf("no prime\n");
+  else{
+  display(prime,n);
+   bubbleSort(prime,n); 
    //insertion(a,N);
 
   // selectionSort(a,N);
-   display(a,N);
+   display(prime,n);
+    }
  return 0;
 }
 
